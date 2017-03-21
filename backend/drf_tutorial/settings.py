d8222c540d2bc8642dfac4346b25ffc87a2d0e97
Leop0ld <l3opold7@gmail.com>
2017-03-22 01:24:10 +0900

    Change many things

--- backend/drf_tutorial/settings.py
+++ backend/drf_tutorial/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '1!y_zw28025hiri32=r&-@oj2850m4wtf*=^#nx@)cgn517jf!'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1', 'localhost']
 
 
 # Application definition
--- frontend/drf_tutorial/src/App.js
+++ frontend/drf_tutorial/src/App.js
@@ -1,21 +1,27 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import axios from 'axios';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+          <button onClick={this.fetchPosts} />
       </div>
     );
   }
+
+  fetchPosts = function () {
+      axios.defaults.headers.post['Content-Type'] = 'application/json';
+      axios.defaults.data['username'] = 'testuser';
+      axios.get('http://127.0.0.1:8000/users/')
+          .then(function(response){
+              console.log(response.data);
+              console.log(response.status);
+          });
+  }
 }
+
+
 
 export default App;
